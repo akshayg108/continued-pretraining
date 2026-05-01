3a194aca10a7b4fde0619322cbb9b25c1915e901
Haodong Zhang <haodoz0118@gmail.com>
2026-05-01 14:46:03 -0400

    Fixed the bug

--- continued_pretraining.py
+++ continued_pretraining.py
@@ -151,7 +151,9 @@ def get_steps_per_epoch(n_samples, batch_size):
 
 
 def create_optim_config(args, warmup_epochs):
-    steps_per_epoch = get_steps_per_epoch(args.n_samples, args.batch_size)
+    accum = max(int(getattr(args, "accumulate_grad_batches", 1)), 1)
+    forwards_per_epoch = get_steps_per_epoch(args.n_samples, args.batch_size)
+    steps_per_epoch = max(forwards_per_epoch // accum, 1)
     total_steps = args.epochs * steps_per_epoch
     warmup_steps = warmup_epochs * steps_per_epoch
     return {
